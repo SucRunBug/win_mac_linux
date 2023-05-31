653150e7c41bccc38a05d6a72441e1086003ef45
SucRunBug <yyanfeiyu@163.com>
2023-05-31 20:14:44 +0800

    add Pizzeria page, find a url bug

--- PythonStudy/pythonProgrammingFromIntro2Practice/Pizzeria/pizza_app/urls.py
+++ PythonStudy/pythonProgrammingFromIntro2Practice/Pizzeria/pizza_app/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'pizza_app'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('pizzas/', views.pizzas, name='pizzas'),
+    path('pizza/<int:pizza_id>/', views.pizza, name='pizza')
 ]
